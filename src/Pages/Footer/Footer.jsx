208845770acf385dfb07dec31741e8c5ae0e10ee
Bappi1234 <harunbappi1234@gmail.com>
2025-01-04 21:46:35 +0600

    footer impliment

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -1,9 +1,59 @@
-
-
+import { FaFacebook, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo2.png";
 export default function Footer() {
   return (
     <div>
-      <h1>footer</h1>
+      <footer className="footer bg-neutral text-neutral-content p-4 items-center">
+        <aside className="flex items-center">
+          <img className="w-20" src={logo} alt="" />
+          <h2 className="text-2xl">
+            Harunur{" "}
+            <span className="text-orange-600" title={"Rosid"}>
+              R.
+            </span>
+          </h2>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Social</h6>
+          {/* Social Icon */}
+          <div className="flex items-center gap-3">
+            <Link
+              to="https://github.com/HarunBappi"
+              target="_blank"
+              className="border-2 border-orange-400 rounded-full"
+            >
+              <FaGithub className="text-white text-3xl p-1"></FaGithub>
+            </Link>
+            <Link
+              to="https://www.linkedin.com/in/md-harunur-rashid-bappi-99230412a/"
+              target="_blank"
+              className="border-2 border-orange-400 rounded-full"
+            >
+              <FaLinkedinIn className="text-white text-3xl p-1"></FaLinkedinIn>
+            </Link>
+            <Link
+              to="https://www.facebook.com/harunbappi0110/"
+              target="_blank"
+              className="border-2 border-orange-400 rounded-full"
+            >
+              <FaFacebook className="text-white text-3xl p-1"></FaFacebook>
+            </Link>
+          </div>
+        </nav>
+      </footer>
+      <footer className="footer footer-center bg-base-300 text-base-content p-2">
+        <aside>
+          <p className="flex items-center gap-2">
+            Copyright © {new Date().getFullYear()} - All right reserved by <h2 className="text-sm">
+            Harunur{" "}
+            <span className="text-orange-600" title={"Rosid"}>
+              R.
+            </span>
+          </h2>
+          </p>
+        </aside>
+      </footer>
     </div>
-  )
+  );
 }
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,26 +1,52 @@
+import { easeOut } from "motion";
+import { motion } from "motion/react";
+import { FaFacebook, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import bannerImage from "../../../assets/harun.png";
-import "../../../index.css";
 export default function Banner() {
   return (
-    <div className="bg-gray-100 h-screen flex items-center relative">
-      <div className="w-1/2 bg-blue-300 h-full flex flex-col justify-center p-10">
-        <h2 className="text-white text-4xl font-bold">Hi, I'm Harunur Rosid</h2>
+    <div className="bg-gray-100 h-[550px] flex items-center relative">
+      <div className="w-1/2 bg-[#2C3E50] h-full flex flex-col justify-center p-10">
+        <motion.h2
+        animate={{ x: 50, color: ["#ff6133", "#FFFF"] }}
+        transition={{
+          duration: 2,
+          delay: 1,
+          ease: easeOut,
+          repeat: Infinity,
+        }}
+         className="text-5xl font-bold">Hi, I'm Harunur Rosid</motion.h2>
+        <h3 className="text-orange-600 mt-4 text-3xl font-bold">
+        Frontend Developer
+        </h3>
         <p className="text-white mt-4">
-          Front End Developer Crafting beautiful & functional Web Experience
+        Creating Fast, Responsive and User-Centered Web Experience.
         </p>
+        {/* Social Icon */}
+        <div className="flex items-center gap-3 mt-5">
+            <Link to='https://github.com/HarunBappi' target="_blank" className="border-2 border-orange-400 rounded-full">
+               <FaGithub className="text-white text-3xl p-1"></FaGithub>
+            </Link>
+            <Link to='https://www.linkedin.com/in/md-harunur-rashid-bappi-99230412a/' target="_blank" className="border-2 border-orange-400 rounded-full">
+                <FaLinkedinIn className="text-white text-3xl p-1"></FaLinkedinIn>
+            </Link> 
+            <Link to='https://www.facebook.com/harunbappi0110/' target="_blank" className="border-2 border-orange-400 rounded-full">
+             <FaFacebook className="text-white text-3xl p-1"></FaFacebook>
+            </Link> 
+        </div>
       </div>
       {/* right */}
-      <div className="w-1/2 h-full bg-[#011324] relative overflow-hidden">
+      <div className="w-1/2 h-full bg-[#ECF0F1] relative overflow-hidden">
         {/* Shape */}
         <div
-          className="absolute top-0 left-0  h-full w-3/4  bg-[#001F3F]"
-          style={{ "clip-path": "polygon(100% 35%, 100% 0, 0 0, 0% 100%)" }}
+          className="absolute top-0 left-0  h-full w-full  bg-[#ebecec]"
+          style={{ "clip-path": "polygon(15% 100%, 100% 0, 0 0, 0% 100%)" }}
         ></div>
-        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center ">
+        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-end ">
           <img
             src={bannerImage}
             alt=""
-            className="w-3/4 object-cover rounded-lg shadow-lg"
+            className="w-3/4"
           />
         </div>
       </div>
